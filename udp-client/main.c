cfdc707705a7062c7df158fce913422c53d02d56
Ankeet Presswala <mythic.boost@gmail.com>
2012-09-02 18:12:13 -0400

    Initial Commit

---
+++ udp-client/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  udp-client
+//
+//  Created by Ankeet Presswala on 9/2/12.
+//  Copyright (c) 2012 University of Maryland. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
